79e8e3d0186f8cc9235dde3957fc3d5093db7ac2
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-30 12:44:19 +0200

    TP 2 terminé

---
+++ src/main/java/app/main/Livre.java
@@ -0,0 +1,19 @@
+package app.main;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="livre")
+public class Livre {
+	@Id private Integer id;
+	@Column(name="TITRE", length=255,nullable=false)
+	private String titre;
+	@Column(name="AUTEUR", length=50,nullable=false)
+	private String auteur;
+	
+	public Livre(){	
+	}
+}
--- src/main/java/app/main/TestJpa.java
+++ src/main/java/app/main/TestJpa.java
@@ -2,7 +2,9 @@ package app.main;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,5 +17,13 @@ public class TestJpa {
 		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
 		EntityManager em = entityManagerFactory.createEntityManager();
+		
+		EntityTransaction transac = em.getTransaction();
+		transac.begin();
+		
+		Livre livre1 = em.find(Livre.class, 1);
+		Query query = em.createQuery("select livre from Livre livre where livre.titre='Germinal'");
+		Livre livre2 = (Livre) query.getResultList().get(0);
+		
 	}
 }
