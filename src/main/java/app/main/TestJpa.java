7f203f0cc0deb7f3d9191ed49225d5905997951a
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-30 11:12:46 +0200

    TP 1 terminé

---
+++ src/main/java/app/main/TestJpa.java
@@ -0,0 +1,14 @@
+package app.main;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+		EntityManager em = entityManagerFactory.createEntityManager();
+
+	}
+}
