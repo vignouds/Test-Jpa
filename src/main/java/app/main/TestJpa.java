e3d63856e5235d09653e76549691e3ebe00d90e3
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-30 11:48:11 +0200

    creation d'un logger

--- src/main/java/app/main/TestJpa.java
+++ src/main/java/app/main/TestJpa.java
@@ -4,12 +4,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestJpa {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TestJpa.class);
 
 	public static void main(String[] args) {
 		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
 		EntityManager em = entityManagerFactory.createEntityManager();
-
 	}
 }
