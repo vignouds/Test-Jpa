903bd7bca19d643e0337411f1f09beb18ae79b89
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-05-02 09:56:15 +0200

    ajout de Adresse

--- src/main/java/banque/Client.java
+++ src/main/java/banque/Client.java
@@ -2,6 +2,8 @@ package banque;
 
 import java.time.LocalDate;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -16,4 +18,17 @@ public class Client {
 	private String prenom;
 	@Column(name="dateNiassance", nullable=false)
 	private LocalDate dateNiassance;
+	@Embedded Adresse adresse;
+}
+
+@Embeddable
+class Adresse{
+	@Column(name="numero")
+	private int numero;
+	@Column(name="rue")
+	private String rue;
+	@Column(name="codePostal")
+	private int codePostal;
+	@Column(name="ville")
+	private String ville;
 }
